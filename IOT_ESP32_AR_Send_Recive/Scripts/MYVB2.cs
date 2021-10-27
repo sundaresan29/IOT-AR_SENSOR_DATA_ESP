d7bf34b8922d7fd1402f21e3b76e6567ee791fee
sundaresan29 <52656781+sundaresan29@users.noreply.github.com>
2021-10-27 08:21:59 +0530

    Add files via upload

---
+++ IOT_ESP32_AR_Send_Recive/Scripts/MYVB2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class MYVB2 : MonoBehaviour, IVirtualButtonEventHandler {
+
+    // Use this for initialization
+    public GameObject Offbutton;
+    public WebStreem webstreemdata;
+    void Start() {
+        Offbutton = GameObject.Find("OFF_Button");
+        Offbutton.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    //void getdata0()
+    //{
+        //webstreemdata.Send0();
+    //}
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        webstreemdata.Send0();
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        
+    }
+}
+
+
