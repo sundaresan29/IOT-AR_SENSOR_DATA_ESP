58c872aa7783bcbaaef6c0470adf9aca3c437394
sundaresan29 <52656781+sundaresan29@users.noreply.github.com>
2021-10-27 12:57:28 +0530

    Update WebStreem.cs

--- IOT_ESP32_AR_Send_Recive/Scripts/WebStreem.cs
+++ IOT_ESP32_AR_Send_Recive/Scripts/WebStreem.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+//Created By    : Sundaresan S
+//Project       : To Send and Recive Data from adafruit io cloud 
+//Date          : 10 April 2019
+//Description   :This is a code for recive any data from cloud and also publish 
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
